785666ddfe470e2d3ca2ea35cb5847cffd26b6a8
EldosB <eldosbejsenbaj2@gmail.com>
2024-12-09 10:48:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const E_Shop = [
+  { name: "Magnum" },
+  { phoneNumber: 87709905555 },
+  { productName: "Fanta0,5", price: 320 },
+  { productName: "snickers", price: 270 },
+  { productName: "Lays", price: 790 },
+  { productName: "tictac", price: 300 },
+  { productName: "kinder", price: 490 },
+  { stock: "Fanta0,5", block: 470 },
+  { stock: "snickers", korobka: 500 },
+  { stock: "Lays", korobka: 750 },
+  { stock: "tictac", shtuk: 500 },
+  { stock: "kinder", shtuk: 700 },
+];
